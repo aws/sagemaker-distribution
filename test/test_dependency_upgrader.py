addad25d1d59a3d0bf9437ce8d00716e765b4e7b
Balaji Sankar <sankarbs@amazon.com>
2023-09-05 17:17:57 -0500

    fix: fix env.in version constraints for major version upgrade

--- test/test_dependency_upgrader.py
+++ test/test_dependency_upgrader.py
@@ -15,18 +15,18 @@ from dependency_upgrader import (
 
 def test_get_dependency_upper_bound_for_runtime_upgrade():
     # case 1: Runtime upgrade type is MINOR, dependency name is python => use pythonesque
-    assert _get_dependency_upper_bound_for_runtime_upgrade('python', '1.2.5', _MINOR) == '<1.3.0'
+    assert _get_dependency_upper_bound_for_runtime_upgrade('python', '1.2.5', _MINOR) == ',<1.3.0'
     # case 1: Runtime upgrade type is MINOR, dependency name is node => use semver
-    assert _get_dependency_upper_bound_for_runtime_upgrade('node', '1.2.5', _MINOR) == '<2.0.0'
+    assert _get_dependency_upper_bound_for_runtime_upgrade('node', '1.2.5', _MINOR) == ',<2.0.0'
 
 
 def test_get_dependency_upper_bound_for_semver():
     # case 1: Runtime upgrade type is MAJOR.
     assert _get_dependency_upper_bound_for_semver('1.2.5', _MAJOR) == ''
     # case 2: Runtime upgrade type is MINOR.
-    assert _get_dependency_upper_bound_for_semver('1.2.5', _MINOR) == '<2.0.0'
+    assert _get_dependency_upper_bound_for_semver('1.2.5', _MINOR) == ',<2.0.0'
     # case 3: Runtime upgrade type is PATCH.
-    assert _get_dependency_upper_bound_for_semver('1.2.5', _PATCH) == '<1.3.0'
+    assert _get_dependency_upper_bound_for_semver('1.2.5', _PATCH) == ',<1.3.0'
     # case 4: Throw exception for any other runtime_upgrade_type
     with pytest.raises(Exception):
         _get_dependency_upper_bound_for_semver('1.2.5', 'prerelease')
@@ -36,9 +36,9 @@ def test_get_dependency_upper_bound_for_pythonesque():
     # case 1: Runtime upgrade type is MAJOR.
     assert _get_dependency_upper_bound_for_pythonesque('1.2.5', _MAJOR) == ''
     # case 2: Runtime upgrade type is MINOR.
-    assert _get_dependency_upper_bound_for_pythonesque('1.2.5', _MINOR) == '<1.3.0'
+    assert _get_dependency_upper_bound_for_pythonesque('1.2.5', _MINOR) == ',<1.3.0'
     # case 3: Runtime upgrade type is PATCH. For pythonesque, we will bump minor version.
-    assert _get_dependency_upper_bound_for_pythonesque('1.2.5', _PATCH) == '<1.3.0'
+    assert _get_dependency_upper_bound_for_pythonesque('1.2.5', _PATCH) == ',<1.3.0'
     # case 4: Throw exception for any other runtime_upgrade_type
     with pytest.raises(Exception):
         _get_dependency_upper_bound_for_pythonesque('1.2.5', 'prerelease')
