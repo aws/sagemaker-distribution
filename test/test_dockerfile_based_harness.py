f12c68ee28cabf569bcf0f368bcca9e3bb3b2056
Jason Weill <jweill@amazon.com>
2023-10-06 16:33:01 +0000

    Adds altair package to tests

--- test/test_dockerfile_based_harness.py
+++ test/test_dockerfile_based_harness.py
@@ -30,7 +30,9 @@ _docker_client = docker.from_env()
     ("jupyter-ai.test.Dockerfile", ['jupyter-ai']),
     ("jupyter-lsp.test.Dockerfile", ['jupyter-lsp']),
     ("jupyter-lsp-server.test.Dockerfile", ['jupyter-lsp-server']),
-    ("notebook.test.Dockerfile", ['notebook'])])
+    ("notebook.test.Dockerfile", ['notebook']),
+    ("altair.test.Dockerfile", ['altair'])])
+>>>>>>> 222bca5 (Adds altair package to tests)
 def test_dockerfiles_for_cpu(dockerfile_path: str, required_packages: List[str],
                              local_image_version: str, use_gpu: bool):
     _validate_docker_images(dockerfile_path, required_packages, local_image_version, use_gpu, 'cpu')
@@ -51,7 +53,8 @@ def test_dockerfiles_for_cpu(dockerfile_path: str, required_packages: List[str],
     ("jupyter-ai.test.Dockerfile", ['jupyter-ai']),
     ("jupyter-lsp.test.Dockerfile", ['jupyter-lsp']),
     ("jupyter-lsp-server.test.Dockerfile", ['jupyter-lsp-server']),
-    ("notebook.test.Dockerfile", ['notebook'])])
+    ("notebook.test.Dockerfile", ['notebook']),
+    ("altair.test.Dockerfile", ['altair'])])
 def test_dockerfiles_for_gpu(dockerfile_path: str, required_packages: List[str],
                              local_image_version: str, use_gpu: bool):
     _validate_docker_images(dockerfile_path, required_packages, local_image_version, use_gpu, 'gpu')
