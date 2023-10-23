376716cf49cc9270b8cfdb6f6cdb0a05063558f6
Navin Soni <navinsoni89@gmail.com>
2023-10-23 09:10:52 -0700

    Add support for SageMaker Studio Analytics Extension (#101)
    Co-authored-by: Navin Soni <navinns@amazon.com>

--- test/test_dockerfile_based_harness.py
+++ test/test_dockerfile_based_harness.py
@@ -26,7 +26,8 @@ _docker_client = docker.from_env()
     ("pandas.test.Dockerfile", ['pandas']),
     ("sm-python-sdk.test.Dockerfile", ['sagemaker-python-sdk']),
     ("pytorch.examples.Dockerfile", ['pytorch']),
-    ("tensorflow.examples.Dockerfile", ['tensorflow'])])
+    ("tensorflow.examples.Dockerfile", ['tensorflow']),
+    ("sagemaker-studio-analytics-extension.test.Dockerfile", ['sagemaker-studio-analytics-extension'])])
 def test_dockerfiles_for_cpu(dockerfile_path: str, required_packages: List[str],
                              local_image_version: str, use_gpu: bool):
     _validate_docker_images(dockerfile_path, required_packages, local_image_version, use_gpu, 'cpu')
@@ -43,7 +44,8 @@ def test_dockerfiles_for_cpu(dockerfile_path: str, required_packages: List[str],
     ("pandas.test.Dockerfile", ['pandas']),
     ("sm-python-sdk.test.Dockerfile", ['sagemaker-python-sdk']),
     ("pytorch.examples.Dockerfile", ['pytorch']),
-    ("tensorflow.examples.Dockerfile", ['tensorflow'])])
+    ("tensorflow.examples.Dockerfile", ['tensorflow']),
+    ("sagemaker-studio-analytics-extension.test.Dockerfile", ['sagemaker-studio-analytics-extension'])])
 def test_dockerfiles_for_gpu(dockerfile_path: str, required_packages: List[str],
                              local_image_version: str, use_gpu: bool):
     _validate_docker_images(dockerfile_path, required_packages, local_image_version, use_gpu, 'gpu')
