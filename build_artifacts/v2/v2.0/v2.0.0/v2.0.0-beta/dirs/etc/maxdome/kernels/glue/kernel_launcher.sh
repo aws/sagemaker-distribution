b192792e5414e3f9b8efd53cc3917e2b8ed737de
Ashley <10680839+AshleyZSY@users.noreply.github.com>
2024-03-29 22:00:13 +0000

    Modified kernel_launcher
    Test:
    Did a %env in a glue kernel and verified glue_connection is there.

--- build_artifacts/v2/v2.0/v2.0.0/v2.0.0-beta/dirs/etc/maxdome/kernels/glue/kernel_launcher.sh
+++ build_artifacts/v2/v2.0/v2.0.0/v2.0.0-beta/dirs/etc/maxdome/kernels/glue/kernel_launcher.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-check_key() {
+export_if_key_exists() {
     local json="$1"
     local full_key="$2"
+    local env_var_name="$3"
 
     # Split the full key into an array
     IFS='.' read -r -a key_parts <<< "$full_key"
@@ -18,7 +19,9 @@ check_key() {
         current_json=$(jq --arg key "$key" '.[$key]' <<< "$current_json")
     done
 
-    return 0  # Key exists
+    # Key exists
+    export "$env_var_name"="$current_json"
+    return 0  
 }
 
 kernel_type=$2
@@ -28,32 +31,6 @@ glue_connection=`maxdome-toolkit connection read -n $MAXDOME_CONNECTION_NAME -ws
 export glue_role_arn=$(echo "$glue_connection" | jq .environmentRoleArn -r)
 export AWS_REGION=$(echo "$glue_connection" | jq .location.awsRegion -r)
 
-if check_key "$glue_connection" "sparkGlueProperties.glueConnection.Name"; then
-    export glue_connections=$(echo "$glue_connection" | jq .sparkGlueProperties.glueConnection.Name -r)
-fi
-
-if check_key "$glue_connection" "sparkGlueProperties.sessionConfigs.glue_version"; then
-    export glue_version=$(echo "$glue_connection" | jq .sparkGlueProperties.sessionConfigs.glue_version -r)
-fi
-
-if check_key "$glue_connection" "sparkGlueProperties.sessionConfigs.spark_conf"; then
-    export glue_spark_conf=$(echo "$glue_connection" | jq .sparkGlueProperties.sessionConfigs.spark_conf -r)
-fi
-
-if check_key "$glue_connection" "sparkGlueProperties.sessionConfigs.session_type"; then
-    export glue_session_type=$(echo "$glue_connection" | jq .sparkGlueProperties.sessionConfigs.session_type -r)
-fi
-
-if check_key "$glue_connection" "sparkGlueProperties.sessionConfigs.extra_py_files"; then
-    export extra_py_files=$(echo "$glue_connection" | jq .sparkGlueProperties.sessionConfigs.session_type -r)
-fi
-
-if check_key "$glue_connection" "sparkGlueProperties.sessionConfigs.additional_python_modules"; then
-    export additional_python_modules=$(echo "$glue_connection" | jq .sparkGlueProperties.sessionConfigs.session_type -r)
-fi
-
-if check_key "$glue_connection" "sparkGlueProperties.sessionConfigs.extra_jars"; then
-    export extra_jars=$(echo "$glue_connection" | jq .sparkGlueProperties.sessionConfigs.session_type -r)
-fi
+export_if_key_exists "$glue_connection" "sparkGlueProperties.glueConnection.Name" "glue_connections" 
 
 /opt/conda/bin/python -m "${kernel_type}" -f "${connection_file}"
