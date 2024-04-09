a9a29f6dafb21bf3e5da99f7d81efa91d3e0abb8
Ashley <10680839+AshleyZSY@users.noreply.github.com>
2024-04-09 17:20:19 +0000

    Remove maxdome

--- template/v2/dirs/etc/maxdome/kernels/kernel_launchers/python3_kernel_launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kernel_type=$2
-connection_file=$4
-
-MAXDOME_CONNECTION_NAME=default.iam
-export AWS_REGION=`maxdome get connection --name $MAXDOME_CONNECTION_NAME --authorization-mode PROJECT --with-secret | jq .location.awsRegion -r`
-
-/opt/conda/bin/python -m ${kernel_type} -f ${connection_file}
