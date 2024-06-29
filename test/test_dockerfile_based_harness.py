cecd28a554654f0baaba3666a744e2116685fde1
Ruinong Tian <ruinon@dev-dsk-ruinon-2a-ba2bf164.us-west-2.amazon.com>
2024-06-29 03:54:53 +0000

    move tensorflow and pytorch GPU validation to tests

--- test/test_dockerfile_based_harness.py
+++ test/test_dockerfile_based_harness.py
@@ -45,6 +45,7 @@ _docker_client = docker.from_env()
         ("serve.test.Dockerfile", ["serve-langchain"]),
         ("langchain-aws.test.Dockerfile", ["langchain-aws"]),
         ("mlflow.test.Dockerfile", ["mlflow"]),
+        ("gpu-dependencies.test.Dockerfile", ["pytorch", "tensorflow"]),
     ],
 )
 def test_dockerfiles_for_cpu(
@@ -86,6 +87,7 @@ def test_dockerfiles_for_cpu(
         ("langchain-aws.test.Dockerfile", ["langchain-aws"]),
         ("mlflow.test.Dockerfile", ["mlflow"]),
         ("sagemaker-mlflow.test.Dockerfile", ["sagemaker-mlflow"]),
+        ("gpu-dependencies.test.Dockerfile", ["pytorch", "tensorflow"]),
     ],
 )
 def test_dockerfiles_for_gpu(
