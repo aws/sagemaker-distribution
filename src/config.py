2e07695e3a209c3a8a3a4087e52d9fa2f5269857
Clayton Parnell <crpjr@amazon.com>
2024-11-15 17:58:50 +0000

    Update to CUDA 12.6

--- src/config.py
+++ src/config.py
@@ -80,8 +80,8 @@ _image_generator_configs = {
     3: [
         {
             "build_args": {
-                "TAG_FOR_BASE_MICROMAMBA_IMAGE": "jammy-cuda-12.5.0",
-                "CUDA_MAJOR_MINOR_VERSION": "12.5",  # Should match the previous one.
+                "TAG_FOR_BASE_MICROMAMBA_IMAGE": "jammy-cuda-12.6.0",
+                "CUDA_MAJOR_MINOR_VERSION": "12.6",  # Should match the previous one.
                 "ENV_IN_FILENAME": "gpu.env.in",
                 "ARG_BASED_ENV_IN_FILENAME": "gpu.arg_based_env.in",
             },
