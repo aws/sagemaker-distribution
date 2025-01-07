2bf77404bd1076f40cafd0e3dbf37d0af39b11c1
Samuel Pangestu <pangestu@amazon.com>
2025-01-07 13:51:17 -0800

    Fixes

--- template/v2/dirs/usr/local/bin/merge-settings-util.py
+++ template/v2/dirs/usr/local/bin/merge-settings-util.py
@@ -2,12 +2,11 @@ import json
 from sys import argv
 
 
-# merges json files file1 and file2, overwriting any settings that already exist in file1
-# todo: add error handling and logging
+# merges json files file1 and file2, keys in file2 overwriting any keys that already exist in file1
 def main():
     file1, file2 = argv[1], argv[2]
     # Read JSON data from files
-    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+    with open(file1, "r") as f1, open(file2, "r") as f2:
         data1 = json.load(f1)
         data2 = json.load(f2)
 
@@ -15,7 +14,7 @@ def main():
     merged_data = {**data1, **data2}
 
     # Write the merged data to a new file
-    with open(file2, 'w') as f:
+    with open(file1, "w") as f:
         json.dump(merged_data, f)
 
 
