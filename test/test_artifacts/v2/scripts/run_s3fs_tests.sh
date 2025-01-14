3a779af1a8c8724fc0b8d484d1eac5fde4d90959
Clayton Parnell <crpjr@amazon.com>
2025-01-14 17:44:06 +0000

    fix typo

--- test/test_artifacts/v2/scripts/run_s3fs_tests.sh
+++ test/test_artifacts/v2/scripts/run_s3fs_tests.sh
@@ -8,7 +8,7 @@ git checkout tags/$s3fs_version
 # Install requirements
 # Remove portion incompatible with conda/mamba
 sed -i "s/; python_version < '3.3'//" test_requirements.txt
-# For <2024.12.0, there's no upperbound on moto.
+# For <2024.10.0, there's no upperbound on moto.
 # This causes test failures, so we add the upperbound.
 # https://github.com/fsspec/s3fs/pull/917/files#diff-25d7875059b9486b7912948b78c0bb1c763d565fe0d12dc8287b5f0dc9982176L17
 if [[ "$s3fs_version" == "2024.10.0" ]]; then
