430f4ce6b0100f56876ec32003b61dc55e2eff92
Bhavya Sharma <bhavya2109sharma@gmail.com>
2025-04-09 11:09:01 -0700

    Delete test/test_artifacts/v2/scripts/run_xgboost_gpu_tests.sh
    xgboost_gpu are not running successfully due to errors like no CUDA device available.

--- test/test_artifacts/v2/scripts/run_xgboost_gpu_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-xgboost_gpu_version=$(micromamba list | grep py-xgboost | tr -s ' ' | cut -d ' ' -f 3)
-
-git checkout tags/v$xgboost_gpu_version
-
-python -m pytest tests/python-gpu/test_gpu_basic_models.py -v
-python -m pytest tests/python-gpu/test_gpu_data_iterator.py -v
-python -m pytest tests/python-gpu/test_gpu_prediction.py -v
