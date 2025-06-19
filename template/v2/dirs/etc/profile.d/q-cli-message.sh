be97d25c633a8a6e065c3e669058b8fccf5271db
Manoj Tuguru <manojtug@amazon.com>
2025-06-19 21:08:07 +0000

    Adding Q Messaging in the Terminal

---
+++ template/v2/dirs/etc/profile.d/q-cli-message.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Display Q CLI message when terminal starts
+echo -e echo -e "\033[1;32m
+##############################################################################
+#                                                                            #
+#                               TERMINAL                                     #
+#                         Powered by Amazon Q CLI                            #
+#                                                                            #                                                           #
+#                  © 2025, Amazon Web Services, Inc. or its affiliates.      #
+#                            All rights reserved.                            #
+#                                                                            #
+#                                                                            #
+##############################################################################
+\033[0m"
