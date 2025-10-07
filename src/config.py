44b55d2606de86d5c285a43f284b4736dcc2dcb2
Nageshwari Elango <nagelan@amazon.com>
2025-10-07 13:06:28 +0000

    Handle Amazon Q Agentic Chat version for JupyterLab

--- src/config.py
+++ src/config.py
@@ -1,11 +1,3 @@
-# Optional import of Amazon Q server version configuration
-# If flare_config.py is not available, FLARE_SERVER_VERSION will be None
-# and the build argument will not be added to Docker builds
-try:
-    from flare_config import FLARE_SERVER_VERSION
-except ImportError:
-    FLARE_SERVER_VERSION = None
-
 _image_generator_configs = {
     0: [
         {
@@ -14,7 +6,6 @@ _image_generator_configs = {
                 "CUDA_MAJOR_MINOR_VERSION": "11.8",  # Should match the previous one.
                 "ENV_IN_FILENAME": "gpu.env.in",
                 "ARG_BASED_ENV_IN_FILENAME": "gpu.arg_based_env.in",
-                **({} if FLARE_SERVER_VERSION is None else {"FLARE_SERVER_VERSION": FLARE_SERVER_VERSION}),
             },
             "additional_packages_env_in_file": "gpu.additional_packages_env.in",
             "image_tag_generator": "{image_version}-gpu",
@@ -26,7 +17,6 @@ _image_generator_configs = {
             "build_args": {
                 "TAG_FOR_BASE_MICROMAMBA_IMAGE": "jammy",
                 "ENV_IN_FILENAME": "cpu.env.in",
-                **({} if FLARE_SERVER_VERSION is None else {"FLARE_SERVER_VERSION": FLARE_SERVER_VERSION}),
             },
             "additional_packages_env_in_file": "cpu.additional_packages_env.in",
             "image_tag_generator": "{image_version}-cpu",
@@ -43,7 +33,6 @@ _image_generator_configs = {
                 "ENV_IN_FILENAME": "gpu.env.in",
                 "PINNED_ENV_IN_FILENAME": "gpu.pinned_env.in",
                 "ARG_BASED_ENV_IN_FILENAME": "gpu.arg_based_env.in",
-                **({} if FLARE_SERVER_VERSION is None else {"FLARE_SERVER_VERSION": FLARE_SERVER_VERSION}),
             },
             "additional_packages_env_in_file": "gpu.additional_packages_env.in",
             "image_tag_generator": "{image_version}-gpu",
@@ -56,7 +45,6 @@ _image_generator_configs = {
                 "TAG_FOR_BASE_MICROMAMBA_IMAGE": "jammy",
                 "ENV_IN_FILENAME": "cpu.env.in",
                 "PINNED_ENV_IN_FILENAME": "cpu.pinned_env.in",
-                **({} if FLARE_SERVER_VERSION is None else {"FLARE_SERVER_VERSION": FLARE_SERVER_VERSION}),
             },
             "additional_packages_env_in_file": "cpu.additional_packages_env.in",
             "image_tag_generator": "{image_version}-cpu",
@@ -73,7 +61,6 @@ _image_generator_configs = {
                 "ENV_IN_FILENAME": "gpu.env.in",
                 "PINNED_ENV_IN_FILENAME": "gpu.pinned_env.in",
                 "ARG_BASED_ENV_IN_FILENAME": "gpu.arg_based_env.in",
-                **({} if FLARE_SERVER_VERSION is None else {"FLARE_SERVER_VERSION": FLARE_SERVER_VERSION}),
             },
             "additional_packages_env_in_file": "gpu.additional_packages_env.in",
             "image_tag_generator": "{image_version}-gpu",
@@ -86,7 +73,6 @@ _image_generator_configs = {
                 "TAG_FOR_BASE_MICROMAMBA_IMAGE": "jammy",
                 "ENV_IN_FILENAME": "cpu.env.in",
                 "PINNED_ENV_IN_FILENAME": "cpu.pinned_env.in",
-                **({} if FLARE_SERVER_VERSION is None else {"FLARE_SERVER_VERSION": FLARE_SERVER_VERSION}),
             },
             "additional_packages_env_in_file": "cpu.additional_packages_env.in",
             "image_tag_generator": "{image_version}-cpu",
@@ -103,7 +89,6 @@ _image_generator_configs = {
                 "ENV_IN_FILENAME": "gpu.env.in",
                 "PINNED_ENV_IN_FILENAME": "gpu.pinned_env.in",
                 "ARG_BASED_ENV_IN_FILENAME": "gpu.arg_based_env.in",
-                **({} if FLARE_SERVER_VERSION is None else {"FLARE_SERVER_VERSION": FLARE_SERVER_VERSION}),
             },
             "additional_packages_env_in_file": "gpu.additional_packages_env.in",
             "image_tag_generator": "{image_version}-gpu",
@@ -116,7 +101,6 @@ _image_generator_configs = {
                 "TAG_FOR_BASE_MICROMAMBA_IMAGE": "ubuntu22.04",
                 "ENV_IN_FILENAME": "cpu.env.in",
                 "PINNED_ENV_IN_FILENAME": "cpu.pinned_env.in",
-                **({} if FLARE_SERVER_VERSION is None else {"FLARE_SERVER_VERSION": FLARE_SERVER_VERSION}),
             },
             "additional_packages_env_in_file": "cpu.additional_packages_env.in",
             "image_tag_generator": "{image_version}-cpu",
